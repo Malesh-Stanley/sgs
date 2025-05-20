81a0a97bbf4047cf4ca2dac80bd3fc98ce5deceb
Malesh-Stanley <111954995+Malesh-Stanley@users.noreply.github.com>
2025-05-20 13:03:41 +0300

    Fixing errors
    Encountered error while deploying on vercel

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Install Python dependencies
-pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 # Create staticfiles directory if it doesn't exist
 mkdir -p staticfiles
